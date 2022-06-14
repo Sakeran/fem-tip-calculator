2246419e309cc0b2c4e93a7ee29eb6a364978b9c
Alexander <Sakeran@gmail.com>
2022-06-14 12:03:02 -0400

    Implement NumberInput component

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,5 @@
-<div>Tip Calculator</div>
+<script lang="ts">
+  import ComponentsLibrary from "./lib/ComponentsLibrary.svelte";
+</script>
+
+<ComponentsLibrary />
---
+++ src/lib/NumberInput.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  export let label: string;
+  export let step: number = 1;
+  export let min: number = 0;
+  export let errorMessage: string = "Invalid Input";
+
+  let input: HTMLInputElement;
+  let id = `number-input-${label.replaceAll(" ", "")}`;
+  let value: number = null;
+  let hasErrors = false;
+
+  let errorDisplay = "";
+  $: {
+    if (hasErrors) {
+      // Use a generic error if the input is not a number
+      if (value === null) {
+        errorDisplay = "Enter a number";
+      } else {
+        errorDisplay = errorMessage;
+      }
+    }
+  }
+
+  const dispatch = createEventDispatcher();
+
+  function handleInput(e) {
+    hasErrors = !input.checkValidity();
+    if (hasErrors || value === null) return;
+
+    dispatch("newvalue", value);
+  }
+</script>
+
+<div class="flex flex-col gap-[0.375rem]">
+  <div class="flex justify-between">
+    <label for={id} class="text-cyan-400">{label}</label>
+    <span class="text-red" class:hidden={!hasErrors}>{errorDisplay}</span>
+  </div>
+  <div class="grid grow">
+    <div
+      class="row-start-1 col-start-1 z-10 self-center ml-5 pointer-events-none"
+    >
+      <slot name="icon" />
+    </div>
+    <div class="row-start-1 col-start-1">
+      <input
+        {id}
+        bind:this={input}
+        bind:value
+        on:input={handleInput}
+        class="min-h-[3rem] w-full text-2xl bg-cyan-1000 text-cyan-100 outline outline-2 outline-white/0  placeholder:text-cyan-100 placeholder:opacity-[0.35] rounded-[0.3125rem] text-right px-4"
+        type="number"
+        placeholder="0"
+        {min}
+        {step}
+      />
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  input {
+    appearance: textfield;
+    transition: outline-color 150ms ease-in-out;
+  }
+
+  input:invalid {
+    @apply outline-red;
+  }
+
+  input:not(:invalid):focus-visible {
+    @apply outline-cyan-500;
+  }
+</style>
