189d61de3ac41a39ef1b7bcdc19dca0ca4060ebb
Alexander <Sakeran@gmail.com>
2022-06-16 16:53:44 -0400

    Give status roll to TotalDisplay

--- src/lib/TotalDisplay.svelte
+++ src/lib/TotalDisplay.svelte
@@ -5,7 +5,7 @@
   export let value: number;
 </script>
 
-<div class="min-h-[3rem] lg:min-h-[4rem] flex justify-between lg:items-center [--digit-size:2rem] lg:[--digit-size:3rem]">
+<div role="status" aria-live="polite" class="min-h-[3rem] lg:min-h-[4rem] flex justify-between lg:items-center [--digit-size:2rem] lg:[--digit-size:3rem]">
   <div class="leading-tight">
     {title}<br /><span class="text-xs text-cyan-400">/ person</span>
   </div>
